c7318f9b5f9d4bfa2b8ad2d9d8c8c77f779dc119
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-07-02 18:00:02 +0100

    Transaction test 7 failed

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -4,7 +4,7 @@ describe('Transaction Tests', () => {
     let transaction;
 
     beforeEach(() => {
-        transaction = new Transaction(new Date(2012, 0, 10), "deposit", 2000);
+        transaction = new Transaction(new Date(2012, 0, 10).toLocaleDateString("en-GB"), "deposit", 2000);
     })
 
     it('should create an instance of the Transaction class', () => {
@@ -13,7 +13,7 @@ describe('Transaction Tests', () => {
     })
 
     it('should return the correct date of a given transaction', () => {
-        expect(transaction.getDate().toLocaleDateString("en-GB")).toEqual("10/01/2012");
+        expect(transaction.getDate()).toEqual("10/01/2012");
     })
 
     it('should return the correct type of a given transaction', () => {
@@ -26,12 +26,26 @@ describe('Transaction Tests', () => {
 
     it('should return the initial updated balance as undefined', () => {
         expect(transaction.getUpdatedBalance()).toBeUndefined();
-    });
+    })
 
     it('should set the updated balance', () => {
         // Arrange/Act
         transaction.setUpdatedBalance(2000);
 
         expect(transaction.getUpdatedBalance()).toEqual(2000);
+    })
+
+    it('should return the correct details of the transaction', () => {
+        // Arrange/Act
+        const expectedDetails = {
+            date: "10/01/2012",
+            type: "deposit",
+            amount: 2000,
+            updatedBalance: 2000,
+        }
+
+        transaction.setUpdatedBalance(2000);
+
+        expect(transaction.getTransactionDetails()).toEqual(expectedDetails);
     })
 })

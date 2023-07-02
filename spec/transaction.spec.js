b8d5bc81e9dc3f74d399bdd0076b0f79b05ccbb6
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-07-02 16:05:18 +0100

    Transaction test 2 failed

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -4,11 +4,15 @@ describe('Transaction Tests', () => {
     let transaction;
 
     beforeEach(() => {
-        transaction = new Transaction();
+        transaction = new Transaction(new Date(2012, 0, 10));
     })
 
     it('should create an instance of the Transaction class', () => {
         expect(transaction).toBeInstanceOf(Transaction);
         expect(transaction).toBeDefined();
+    })
+
+    it('should should return the correct date of a given transaction', () => {
+        expect(transaction.getDate().toLocaleDateString("en-GB")).toEqual("10/01/2012");
     })
 })

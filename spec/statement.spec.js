c616996a0e43a1da8d9daa1ca118f0c1a4327874
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-07-02 20:56:16 +0100

    Statement test 1 failed

---
+++ spec/statement.spec.js
@@ -0,0 +1,16 @@
+describe('Statement Tests', () => {
+    it('should generate the statement for an empty account', () => {
+        // Arrange
+        const account = {
+            getTransactions: () => [],
+        };
+        spyOn(console, "log");
+
+        // Act
+        Statement.generate(account);
+
+        expect(console.log).toHaveBeenCalledWith(
+            'date       || credit  || debit  || balance\n'
+        );
+    })
+})

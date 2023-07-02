48cae2c8ed5c7de8290ce7208218b609afdf6d33
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-07-02 16:06:00 +0100

    Transaction test 2 passed

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,5 +1,19 @@
 class Transaction {
+    #date;
+    #type;
+    #amount;
+    #updatedBalance;
 
+    constructor(date, type, amount) {
+        this.#date = date;
+        this.#type = type;
+        this.#amount = amount;
+        this.#updatedBalance;
+    }
+
+    getDate() {
+        return this.#date;
+    }
 }
 
 export default Transaction;
